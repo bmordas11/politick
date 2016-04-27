8dc42b19470370c855be015b509aabe26bcff942
bmordas11 <brett.mordas@gmail.com>
2016-04-27 11:44:34 -0400

    Fixed Hound errors

--- spec/features/votes/user_vote_comment_spec.rb
+++ spec/features/votes/user_vote_comment_spec.rb
@@ -3,7 +3,7 @@ require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, {js_errors: false})
+  Capybara::Poltergeist::Driver.new(app, js_errors: false)
 end
 
 feature "user upvotes and downvotes a comment" do
